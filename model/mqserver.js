5d862e7ae9e08f63cb995b9d8942e3606ea299eb
Victor S <victsan1168@gmail.com>
2021-03-21 16:25:58 +0100

    Provide echo endpoint to test the connection

--- model/mqserver.js
+++ model/mqserver.js
@@ -1,16 +1,47 @@
 var amqp = require('amqplib/callback_api');
 
-exports.status = function(req,res) {
+function get_uri() {
   const MQHOST = process.env.HOST || "localhost";
   const MQPORT = process.env.MQPORT || 5672;
   const MQUSER = process.env.MQUSER || "guest";
   const MQPASS = process.env.MQPASS || "guest";
-  let uri = `amqp://${MQUSER}:${MQPASS}@${MQHOST}:${MQPORT}/`
+  return `amqp://${MQUSER}:${MQPASS}@${MQHOST}:${MQPORT}/`
+}
 
+exports.status = function(req,res) {
+  var uri = get_uri();
   amqp.connect(uri, function(err, conn) {
-    if(err) {
+    if (err) {
       res.json({status: `NOK: ${err.message}`});
     }
     res.json({status: "OK"});
+  });
+}
+
+exports.echo = function(req,res) {
+  var queue = req.params.queue;
+  var message = req.params.message;
+  var uri = get_uri();
+  amqp.connect(uri, function(err, conn) {
+    if(err) {
+      res.json({status: `Connection refused: ${err.message}`});
+      return;
+    }
+    conn.createChannel(function(err, ch) {
+      if (err){
+        res.json({status: `Error connecting to server: ${err.message}`})
+        return
+      }
+      ch.assertQueue(queue);
+      ch.sendToQueue(queue, Buffer.from(message));
+
+      ch.consume(queue, function(msg) {
+        if (msg !== null) {
+          ch.ack(msg);
+          ch.close();
+          res.json({echo_reply: msg.content.toString()});
+        }
+      });
+    });
   });
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,7 @@ var mqserver = require('../model/mqserver')
 
 exports.assignRoutes = function (app) {
     app.get('/',  mqserver.status);
+    app.post('/echo/:queue/:message', mqserver.echo);
 }
+
+
